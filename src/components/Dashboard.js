e72b42d28789d51db3c59340f868310b6bbbd501
michaelkcwong <wong_michael@rogers.com>
2022-06-19 01:05:35 -0400

    Binding this by altering the selectPanel instance with arrow function, remove id prop and pass ref to the onSelect prop, change selectPanel function to set the value of focused back to null if the value of focused is currently set to panel

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -34,10 +34,10 @@ class Dashboard extends Component {
   };
 
   selectPanel(id) {
-    this.setState({
-     focused: id
-    });
-   }
+    this.setState(previousState => ({
+      focused: previousState.focused !== null ? null:id
+    }));
+  };
 
   render() {
     const dashboardClasses = classnames("dashboard", {
@@ -55,7 +55,7 @@ class Dashboard extends Component {
       id={panel.id}
       label={panel.label}
       value={panel.value}
-      onSelect={this.selectPanel}
+      onSelect={event => this.selectPanel(panel.id)}
      />
     ));
  
